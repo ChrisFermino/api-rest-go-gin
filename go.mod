4dcccd860d3187d72425b51c70a22f5e21314393
Christian-NBW <76758909+ChrisFermino@users.noreply.github.com>
2022-12-05 17:45:22 -0300

    implement dotenv variables

--- go.mod
+++ go.mod
@@ -5,6 +5,7 @@ go 1.19
 require (
 	github.com/dgrijalva/jwt-go v3.2.0+incompatible
 	github.com/gin-gonic/gin v1.8.1
+	github.com/joho/godotenv v1.4.0
 	github.com/swaggo/files v0.0.0-20220728132757-551d4a08d97a
 	github.com/swaggo/gin-swagger v1.5.3
 	github.com/swaggo/swag v1.8.8
--- src/database/db.go
+++ src/database/db.go
@@ -1,23 +1,33 @@
 package database
 
 import (
-    "api-go-gin/src/models"
-    "gorm.io/driver/postgres"
-    "gorm.io/gorm"
-    "log"
+	"api-go-gin/src/models"
+	"github.com/joho/godotenv"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+	"log"
+	"os"
 )
 
+func envVariable(key string) string {
+	err := godotenv.Load(".env")
+	if err != nil {
+		log.Fatalf("error loading .env file")
+	}
+	return os.Getenv(key)
+}
+
 var (
-    DB  *gorm.DB
-    err error
+	DB  *gorm.DB
+	err error
 )
 
 func ConectaComBancoDeDados() {
-    stringDeConexao := "host=localhost user=root password=root dbname=root port=5432 sslmode=disable"
-    DB, err = gorm.Open(postgres.Open(stringDeConexao))
-    if err != nil {
-        log.Panic("Erro ao conectar com banco de dados")
-    }
-    DB.AutoMigrate(&models.Aluno{})
-    DB.AutoMigrate(&models.User{})
+	stringDeConexao := "host=localhost user=" + envVariable("POSTGRES_USER") + " password=" + envVariable("POSTGRES_PASSWORD") + " dbname=" + envVariable("POSTGRES_DB") + " port=" + envVariable("POSTGRES_PORT") + " sslmode=disable"
+	DB, err = gorm.Open(postgres.Open(stringDeConexao))
+	if err != nil {
+		log.Panic("Erro ao conectar com banco de dados")
+	}
+	DB.AutoMigrate(&models.Aluno{})
+	DB.AutoMigrate(&models.User{})
 }
